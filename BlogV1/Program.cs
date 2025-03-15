413ffc1733f6d5f93c7dacf67dd953a51cedae43
Esma Ekim <esmaekim123@gmail.com>
2025-03-15 04:10:29 +0300

    AdminPanel

--- BlogV1/Program.cs
+++ BlogV1/Program.cs
@@ -25,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Blogs}/{action=Index}/{id?}");
+    pattern: "{controller=Admin}/{action=Index}/{id?}");
 
 app.Run();
