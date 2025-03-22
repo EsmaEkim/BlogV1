276dc1d20baade7cb04ee4296eea6765605cd435
Esma Ekim <esmaekim123@gmail.com>
2025-03-23 00:17:34 +0300

    Refactoring

--- BlogV1/Controllers/AdminController.cs
+++ BlogV1/Controllers/AdminController.cs
@@ -2,27 +2,57 @@
 using BlogV1.Identity;
 using BlogV1.Models;
 using BlogV1.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Identity.Client;
-using System.Xml.Linq;
 
 namespace BlogV1.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         private readonly BlogDbContext _context;
         private readonly UserManager<BlogIdentityUser> _userManager;
+        private readonly SignInManager<BlogIdentityUser> _signInManager;
 
-        public AdminController(BlogDbContext context, UserManager<BlogIdentityUser> userManager)
+        public AdminController(BlogDbContext context, UserManager<BlogIdentityUser> userManager, SignInManager<BlogIdentityUser> signInManager)
         {
             _context = context;
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var dashboard = new DashboardViewModel();
+
+            var toplamblogsayisi = _context.Blogs.Count();
+            var toplamgoruntulenme = _context.Blogs.Select(x => x.ViewCount).Sum();
+            var encokgoruntulneneblog = _context.Blogs.OrderByDescending(x => x.ViewCount).FirstOrDefault();
+            var ensonyayinlananblog = _context.Blogs.OrderByDescending(x => x.PublishDate).FirstOrDefault();
+            var toplamyorumsayisi = _context.Comments.Count();
+            var encokyorumalanblogId = _context.Comments
+                                        .GroupBy(x => x.BlogId) // BlogId'ye göre grupla
+                                        .OrderByDescending(g => g.Count()) // Grupları yorum sayısına göre azalan sırala
+                                        .Select(g => g.Key) // En çok yorumu olan BlogId'yi al
+                                        .FirstOrDefault(); // İlk sonucu getir
+            var encokyorumalanblog = _context.Blogs.Where(x => x.Id == encokyorumalanblogId).FirstOrDefault();
+
+            var bugunyapilanyorumsayisi = _context.Comments.Where(x => x.PublishDate.Date == DateTime.Now.Date).Count();
+
+            dashboard.TotalBlogCount = toplamblogsayisi;
+            dashboard.TotalViewCount = toplamgoruntulenme;
+            dashboard.MostViewedBlog = encokgoruntulneneblog;
+            dashboard.LatestBlog = ensonyayinlananblog;
+            dashboard.TotalCommentCount = toplamyorumsayisi;
+            dashboard.MostCommentedBlog = encokyorumalanblog;
+            dashboard.TodayCommentCount = bugunyapilanyorumsayisi;
+
+
+
+
+
+            return View(dashboard);
         }
         public IActionResult Blogs()
         {
@@ -34,6 +64,8 @@ namespace BlogV1.Controllers
             var blog = _context.Blogs.Where(x => x.Id == id).FirstOrDefault();
             return View(blog);
         }
+
+        //blog silme islemi
         public IActionResult DeleteBlog(int id)
         {
             var blog = _context.Blogs.Where(x => x.Id == id).FirstOrDefault();
@@ -41,6 +73,8 @@ namespace BlogV1.Controllers
             _context.SaveChanges();
             return RedirectToAction("Blogs");
         }
+
+        //edit post islemi
         [HttpPost]
         public IActionResult EditBlog(Blog model)
         {
@@ -53,6 +87,7 @@ namespace BlogV1.Controllers
             return RedirectToAction("Blogs");
         }
 
+        //status toogle ac kapa blogu
         public IActionResult ToggleStatus(int id)
         {
             var blog = _context.Blogs.Where(x => x.Id == id).FirstOrDefault();
@@ -88,14 +123,14 @@ namespace BlogV1.Controllers
             var comments = new List<Comment>();
             if (blogId == null)
             {
-               comments = _context.Comments.ToList();
+                comments = _context.Comments.ToList();
             }
             else
             {
-               comments = _context.Comments.Where(x => x.BlogId == blogId).ToList();
+                comments = _context.Comments.Where(x => x.BlogId == blogId).ToList();
             }
-            return View(comments);
 
+            return View(comments);
         }
 
         public IActionResult DeleteComment(int id)
@@ -114,18 +149,17 @@ namespace BlogV1.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel model)
         {
-            if (model.Password==model.RePassword)
+            //burada sifre olusturuken buyuk harf kucuk harf sayi 6 karekter ve simge gerekli
+            if (model.Password == model.RePassword)
             {
                 var user = new BlogIdentityUser
                 {
-                    Name=model.Name,
-                    Surname=model.Surname,
-                    Email=model.Email,
-                    UserName=model.Email    
-                    
-
+                    Name = model.Name,
+                    Surname = model.Surname,
+                    Email = model.Email,
+                    UserName = model.Email,
                 };
-                var result = await _userManager.CreateAsync(user,model.Password);
+                var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
                     return RedirectToAction("Index");
@@ -139,12 +173,18 @@ namespace BlogV1.Controllers
             {
                 return View();
             }
-            
-
         }
-        public IActionResult Login()
+
+        public async Task<IActionResult> Logout()
         {
-            return View();
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Blogs");
+        }
+
+        public IActionResult Contact()
+        {
+            var contact = _context.Contacts.ToList();
+            return View(contact);
         }
     }
 }
